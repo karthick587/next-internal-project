dbc364130148c432e824e6b6ff54a7f869299a09
admin <karthickdurai587@gmail.com>
2021-11-19 17:11:05 +0530

    fgnb

--- components/userdetails.jsx
+++ components/userdetails.jsx
@@ -1,23 +1,12 @@
-import React,{useEffect,useState} from "react";
-import Image from 'next/image'
+import React from "react";
+
 export default function Userdetails(){
 
-    const[data,setData] = useState([]);
-
-    useEffect(() =>{
-        fetch ('https://reqres.in/api/users')
-        .then(res => res.json())
-        .then(res => setData(res.data))
-    }, []); 
+   
     return(
         <div className="container">
           
-             {data.map((item)=>
-                 <div className="row">
-                    
-                     
-                </div>
-           )}  
+           
         
         </div>
     )

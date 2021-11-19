774e33679ec16d7c2bc7b852e52a35878c8f1182
admin <karthickdurai587@gmail.com>
2021-11-19 16:54:58 +0530

    m hjgj

--- components/userdetails.jsx
+++ components/userdetails.jsx
@@ -1,4 +1,5 @@
 import React,{useEffect,useState} from "react";
+import Image from 'next/image'
 export default function Userdetails(){
 
     const[data,setData] = useState([]);
@@ -17,7 +18,7 @@ export default function Userdetails(){
                      <div className="col"> {item.first_name}</div>
                      <div className="col"> {item.last_name}</div>
                      <div className="col"> {item.email}</div>
-                     <div className="col" key={item.id}><img className="rounded-circle"src= {item.avatar} alt="pic1" /></div>
+                     <div className="col" key={item.id}><Image className="rounded-circle"src= {item.avatar} alt="pic1" /></div>
                      
                 </div>
            )}  
